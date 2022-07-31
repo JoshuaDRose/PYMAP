032e5756d28a3cede2c396226719436fc8ea9537
Joshua Rose <101031214+JoshuaDRose@users.noreply.github.com>
2022-07-31 23:39:15 +1000

    Create __main__.py

---
+++ src/__main__.py
@@ -0,0 +1,23 @@
+""" main.py contains main - executes main loop """
+from window import *
+from utils import *
+from player import *
+
+import pygame
+import requests
+import threading
+import console
+
+console_thread = threading.Thread(target=console.do_console, args=())
+console_thread.daemon = True
+console_thread.start()
+
+player = Player()
+window = Window()
+
+def main():
+	window.main_loop()
+
+if __name__ == "__main__":
+	main()
+	
