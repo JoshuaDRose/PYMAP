f85644416312ef7f9c7b4cc7ececb7da6190014d
Joshua Rose <101031214+JoshuaDRose@users.noreply.github.com>
2022-07-31 23:54:33 +1000

    Update INSTALL_PROGRAM.py

--- src/INSTALL_PROGRAM.py
+++ src/INSTALL_PROGRAM.py
@@ -1,6 +1,7 @@
 import subprocess as sp
 try:
   sp.run(['pip', 'install', '-r', 'requirements.txt'])
+  print("Everything has been installed. Please run the __main__.py file either directly or from the command prompt")
 except:
   print("Please install python 3 and ensure that pip is on your system path then re-run this program")
   print("Executable cannot be made on this project as it requires external assets such as png files which do not work with python in py2exe module")
