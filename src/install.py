6cebabdb0bce96acf8be18844473168dd4b56f93
Joshua Rose <101031214+JoshuaDRose@users.noreply.github.com>
2022-08-01 23:25:01 +1000

    Update install.py

--- src/install.py
+++ src/install.py
@@ -1,12 +1,10 @@
 import os
-import re
-import sys
 import utils
 import requests
 
+
 def wget_samples(install_location, url, name):
 	download_data = requests.get(url)
-	# print(f"Installing {name} from {url} into {install_location}")
 	try:
 		with open(os.path.join(install_location,name), 'wb') as f:
 			f.write(download_data.content)
@@ -14,4 +12,4 @@ def wget_samples(install_location, url, name):
 		return;
 	except PermissionError:
 		print("Permission Denied. Please ensure that you run this program as administrator.")
-
+		
